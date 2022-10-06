cc1003d939aca20488e53919fcc8c803b165a843
Demaris Ashmore <demiashmore@yahoo.com>
2022-10-05 22:38:26 -0600

    commit tree 4377090fcd188e685693cd83c5ccbf65fcc0516f

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,29 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  // console.log(movies)
+
+  let displayMovies = movies.map(movie => {
+    return (
+      <div key={movie.title}>
+        <h4>{movie.title}</h4>
+        <p>{movie.time} minutes</p>
+        <ul>
+          {movie.genres.map(genre => {
+            return <li key={genre}>{genre}</li>
+          })}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {displayMovies}
+    </div>
+  )
 }
 
 export default Movies;
